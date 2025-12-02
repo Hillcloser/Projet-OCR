fe41fff46d75e8db0b55ba0fef7447f78c1a4ece
Thibault <138973962+ThibaultDaguier@users.noreply.github.com>
2025-12-02 17:39:30 +0100

    firth test new nn

--- src/nn.c
+++ src/nn.c
@@ -4,20 +4,6 @@
 #include <time.h>
 
 
-
-/*-----------------------------------------------------------------------------
-w = weight,  
-b = bias, 
-
-
-
--------------------------------------------------------------------------------
-input = 0.1 = start.hidden 
-output = 1.2 = hidden.end
------------------------------------------------------------------------------*/
-
-// basic function :
-
 // -1 <= random_d() <= 1
 double random_d()
 {return ((double)rand() / RAND_MAX) * 2 - 1;} 
@@ -25,23 +11,34 @@ double random_d()
 double sigmoid(double z)
 {return 1.0/(1.0 + exp(-z));}
 
-double sigmoid_prime(double z)
+double sigmoid_prime(double z) //Backpropagation of sigmoid
 {return sigmoid(z)*(1-sigmoid(z));}
 
-struct NeuralNetwork 
+void softmax(double *z, double *res)
+{
+	double z_exp_sum = 0;
+	for (int e_i = 0; e_i < 26; e_i++) z_exp_sum += exp(z[e_i]);
+	for (int e_i = 0; e_i < 26; e_i++) res[e_i] = exp(z[e_i])/z_exp_sum;
+}
+
+double softmax_B(double res, double y) //Backpropagation of softmax
+{return res - y;}
+
+
+struct NeuralNetwork
 {
 	int n; //hidden len
 	
-	double **input_w;
-	double *output_w;
+	double **input_w; // 26*n
+	double **output_w;// n*784
 	
-	double *hidden_b;
+	double *hidden_b; 
 	double *hidden_z;
 	double *hidden_res;
 	
-	double end_b;
-	double end_z;
-	double end_res;
+	double *end_b;
+	double *end_z;
+	double *end_res; 
 	
 	double learning_rate;
 };
@@ -51,143 +48,140 @@ struct NeuralNetwork init_NN(int n, double learning_rate)
 	struct NeuralNetwork res;
 	
 	res.n = n;
+	///// init memory
+	//[p_i][h_i]
+	res.input_w = calloc(784, sizeof(double *));					
+	for (int p_i = 0 ; p_i < 784; p_i++) res.input_w[p_i] = calloc(res.n, sizeof(double));
 	
-	res.input_w = calloc(2, sizeof(double *));
-	res.input_w[0] = calloc(res.n, sizeof(double));
-	res.input_w[1] = calloc(res.n, sizeof(double));
+	//[h_i][e_i]
+	res.output_w = calloc(res.n, sizeof(double *));
+	for (int h_i = 0 ; h_i < res.n; h_i++) res.output_w[h_i] = calloc(26, sizeof(double));
 	
-	res.output_w = calloc(res.n, sizeof(double));
-	
+	//[h_i]
 	res.hidden_b = calloc(res.n, sizeof(double));
-	res.hidden_z = calloc(res.n, sizeof(double));;
+	res.hidden_z = calloc(res.n, sizeof(double));
 	res.hidden_res = calloc(res.n, sizeof(double));
 	
-	res.end_b = random_d();
+	//[e_i]
+	res.end_b = calloc(26, sizeof(double));
+	res.end_z = calloc(26, sizeof(double));
 	
 	res.learning_rate = learning_rate;
+	/////
 	
-	for (int i = 0 ; i < res.n; i++)
-	{
-		for (int j = 0; j < 2 ; j++) res.input_w[j][i] = random_d();
-		res.output_w[i] = random_d();
-		res.hidden_b[i] = random_d(); 
-	}
+	///// init val
+	for (int h_i = 0 ; h_i < res.n; h_i++) res.hidden_b[h_i] = random_d(); 
+	for (int e_i = 0; e_i < 26 ; e_i++) res.end_b[e_i] = random_d(); 
+	
+	for (int p_i = 0; p_i < 784 ; p_i++) for (int h_i = 0 ; h_i < res.n; h_i++) res.output_w[p_i][h_i] = random_d();
+	for (int h_i = 0 ; h_i < res.n; h_i++) for (int e_i = 0; e_i < 26 ; e_i++) res.input_w[h_i][e_i] = random_d();
+	/////
+	
 	return res;
 }
 
 void free_NN(struct NeuralNetwork *nn)
 {
-	free(nn -> input_w[0]);
-	free(nn -> input_w[1]);
-	free(nn -> input_w);
-	free(nn -> output_w);
-	free(nn -> hidden_b);
-	free(nn -> hidden_z);
-	free(nn -> hidden_res);
-}
-
-double forward(struct NeuralNetwork *nn, int A, int B)
-{
-	int i = 0;
-	while (i < nn -> n)
-	{
-		nn -> hidden_z[i] = nn -> input_w[0][i] * (double)A
-			+ nn -> input_w[1][i] * (double)B
-			+ nn -> hidden_b[i];
-		i++;
-	}
-	i = 0;
-	nn -> end_z = nn -> end_b;
-	while (i < nn -> n)
-	{
-		nn -> hidden_res[i] = sigmoid(nn -> hidden_z[i]);
-		nn -> end_z += nn -> output_w[i] * nn -> hidden_res[i];
-		i++;
-	}
-	nn -> end_res = sigmoid(nn -> end_z);
+						
+	for (int h_i = 0 ; h_i < nn->n; h_i++) free(nn->input_w[h_i]);
+	free(nn->input_w);
 	
-	return nn -> end_res;
+	for (int e_i = 0; e_i < 26 ; e_i++) free(nn->output_w[e_i]);
+	free(nn->output_w);
+	
+	free(nn->hidden_b);
+	free(nn->hidden_z);
+	free(nn->hidden_res);
+	
+	free(nn->end_b);
+	free(nn->end_z);
+	free(nn->end_res);
 }
 
-void backward(struct NeuralNetwork *nn, int A, int B, double f, int e)
+char forward(struct NeuralNetwork *nn, int *pixel) //return for backward in NN
 {
-	double end_delta = ((double)e - f)
-		* sigmoid_prime(nn -> end_z);
-	double *hidden_delta = calloc(nn -> n, sizeof(double));
-	for (int i = 0 ; i < nn -> n; i++)
-	{
-		hidden_delta[i] = end_delta 
-				* nn -> output_w[i]
-				* sigmoid_prime(nn -> hidden_z[i]);
-
-
-		nn -> output_w[i] += nn -> learning_rate 
-				* end_delta
-				* nn -> hidden_res[i];
+	for (int h_i = 0; h_i < nn->n ; h_i++) 
+		nn->hidden_z[h_i] = nn->hidden_b[h_i];
+	
+	for (int p_i = 0; p_i < 784 ; p_i++) 
+		for (int h_i = 0; h_i < nn->n ; h_i++) 
+			nn->hidden_z[h_i] += nn->input_w[p_i][h_i] * (double)pixel[p_i]; 
 		
-
-
-		nn -> input_w[0][i] += nn -> learning_rate
-			* hidden_delta[i] * (double)A;
-		nn -> input_w[1][i] += nn -> learning_rate
-			* hidden_delta[i] * (double)B;
-		nn -> hidden_b[i] += nn -> learning_rate
-			* hidden_delta[i];
-	}
+	for (int h_i = 0; h_i < nn->n ; h_i++) 
+		nn->hidden_res[h_i] = sigmoid(nn->hidden_z[h_i]);
 	
-	nn -> end_b += nn -> learning_rate * end_delta;
+	for (int e_i = 0; e_i < 26 ; e_i++) 
+		nn->end_z[e_i] = nn->end_b[e_i];
 	
+	for (int h_i = 0; h_i < nn->n ; h_i++) 
+		for (int e_i = 0; e_i < 26 ; e_i++) 
+			nn->end_z[e_i] += nn->output_w[h_i][e_i] * nn->hidden_res[h_i]; 
+	
+	softmax(nn->end_z, nn->end_res);
+	
+	int res = 0;
+	for (int e_i = 1; e_i < 26 ; e_i++) if (nn->end_res[e_i] > nn->end_res[res]) res = e_i;
+	
+	return 'A'+res;
+}
+
+void backward(struct NeuralNetwork *nn, int *pixel, int *e) //
+{
+	double *end_delta = calloc(26, sizeof(double));
+	double **hidden_delta = calloc(nn->n, sizeof(double));
+	
+	for (int e_i = 0 ; e_i < 26; e_i++)
+		end_delta[e_i] = softmax_B(nn->end_res[e_i], e[e_i]));
+	for (int e_i = 0 ; e_i < 26; e_i++)
+		nn -> end_b[h_i] += nn -> learning_rate * end_delta[h_i];
+	for (int h_i = 0 ; h_i < nn -> n; h_i++) for (int e_i = 0 ; e_i < 26; e_i++)  
+		nn -> output_w[h_i][e_i] += nn -> learning_rate * end_delta * nn -> hidden_res[h_i];
+	
+	for (int h_i = 0 ; h_i < nn -> n; h_i++)
+		hidden_delta[h_i] = end_delta * nn -> output_w[h_i] * sigmoid_prime(nn -> hidden_z[h_i]);
+	for (int h_i = 0 ; h_i < nn -> n; h_i++) 
+		nn -> hidden_b[h_i] += nn -> learning_rate * hidden_delta[h_i];
+	for (int p_i = 0 ; p_i < 784; p_i++) for (int h_i = 0 ; h_i < nn -> n; h_i++) 
+		nn -> input_w[p_i][h_i] += nn -> learning_rate * hidden_delta[h_i] * (double)pixel[p_i];
+	
+	free(end_delta);
 	free(hidden_delta);
 }
 
-void train(struct NeuralNetwork *nn, int loop)
+void train(struct NeuralNetwork *nn, int loop) //
 {
-	double tmp;
-	for (int i = 0 ; i < loop; i++)
+	int *abc = calloc(26, sizeof(int));
+	for (int letter = 0 ; letter < 26; letter++)//A - Z
 	{
-		tmp = forward(nn, 0, 0);
-		backward(nn, 0, 0, tmp, 1);
-		tmp = forward(nn, 0, 1);
-		backward(nn, 0, 1, tmp, 0);
-		tmp = forward(nn, 1, 0);
-		backward(nn, 1, 0, tmp, 0);
-		tmp = forward(nn, 1, 1);
-		backward(nn, 1, 1, tmp, 1);
+		abc[letter] = 1;
+		
+		forward(
+				nn, 
+				image[letter]
+				);
+				
+		backward(
+				nn,
+				image[letter], 
+				abc
+				);
+				
+		abc[Letter] = 0;
 	}
+	free(abc);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) //
 {
 	srand(time(NULL));
-	struct NeuralNetwork NN = init_NN(3, 0.1);
+	struct NeuralNetwork NN = init_NN(3, 0.5);
 	if (argc == 1) 
 	{
 		train(&NN, 100000);
 		double res = forward(&NN, 0, 0);
-		printf("forward((A=%i), (B=%i)) = %f ≈ %i\n",
+		printf("forward(Letter = ) = %f ≈ %i\n",
 			0,
 			0,
-			res,
-			res > 0.5
-			);
-		res = forward(&NN, 0, 1);
-		printf("forward((A=%i), (B=%i)) = %f ≈ %i\n",
-			0,
-			1,
-			res,
-			res > 0.5
-			);
-		res = forward(&NN, 1, 0);
-		printf("forward((A=%i), (B=%i)) = %f ≈ %i\n",
-			1,
-			0,
-			res,
-			res > 0.5
-			);
-		res = forward(&NN, 1, 1);
-		printf("forward((A=%i), (B=%i)) = %f ≈ %i\n",
-			1,
-			1,
 			res,
 			res > 0.5
 			);
@@ -195,7 +189,7 @@ int main(int argc, char *argv[])
 	else if (argc == 3)
 	{
 		train(&NN, 100000);
-		double res = forward(&NN, atoi(argv[1]), atoi(argv[2]));
+		double res = forward(&NN, test);
 		printf("forward((A=%i), (B=%i)) = %f ≈ %i\n",
 				atoi(argv[1]),
 				atoi(argv[2]),
@@ -206,7 +200,7 @@ int main(int argc, char *argv[])
 	else if (argc == 4)
 	{
 		train(&NN, atoi(argv[3]));
-		double res = forward(&NN, atoi(argv[1]), atoi(argv[2]));
+		double res = forward(&NN, test);
 		printf("forward((A=%i), (B=%i)) = %f ≈ %i\n",
 				atoi(argv[1]),
 				atoi(argv[2]),
@@ -221,30 +215,21 @@ int main(int argc, char *argv[])
 	free_NN(&NN);
 }
 
-/*
-int main(int argc, char *argv[])
-{
-	srand(time(NULL));
-	struct NeuralNetwork NN = init_NN(3, 0.1);
-	double res;
-	int rr = 1;
-	train(&NN, 100000);
-	for (int i = 0 ; i < 100000000000; i++)
-	{
-	    res = forward(&NN, 0, 0);
-	    rr = rr && (1 == (res > 0.5));
-	    printf("%i",(res > 0.5));
-	    res = forward(&NN, 0, 1);
-		rr = rr && (0 == (res > 0.5));
-		printf("%i",(res > 0.5));
-		res = forward(&NN, 1, 0);
-		rr = rr && (0 == (res > 0.5));
-		printf("%i",(res > 0.5));
-		res = forward(&NN, 1, 1);
-		rr = rr && (1 == (res > 0.5));
-		printf("%i\n",(res > 0.5));
-	}
-	printf("\n%i",rr);
-	free_NN(&NN);
-}
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
